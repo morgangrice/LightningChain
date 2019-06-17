01e7eeffc5023fa70d4dbabaaa6a3e316ce7b820
Morgan Grice <morgan.grice@outlook.com>
2019-06-17 22:09:59 +0800

    Backend test cases

--- blockchain.js
+++ blockchain.js
@@ -1,7 +1,8 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain{
-    constructor(){
+    constructor() {
         this.chain = [Block.genesis()];
     }
 
@@ -11,6 +12,41 @@ class Blockchain{
         });
     
         this.chain.push(newBlock);
+    }
+
+    replaceChain(chain){
+        if (chain.length <= this.chain.length) {
+            console.error('The incoming chain must be longer!');
+            return;
+        }
+
+        if (!Blockchain.isValidChain(chain)) {
+            console.error('The incoming chain must be valid!');
+            return;
+        }
+
+        console.log('Replacing chain with', chain);
+        this.chain = chain;
+    }
+
+    static isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false;
+        };
+        
+        for (let i=1; i<chain.length; i++) {
+            const { timestamp, lastHash, hash, data } = chain[i];
+            
+            const actualLastHash =  chain[i-1].hash;           
+
+            if (lastHash !== actualLastHash) return false;
+
+            const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+            if (hash !== validatedHash) return false;
+        }
+
+        return true;
     }
 }
 
